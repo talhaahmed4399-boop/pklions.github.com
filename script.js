2fdc82ff2f355314a7425a62da421dc3903245c0
talhaahmed4399-boop <talhaahmed4399@gmail.com>
2026-02-13 12:50:37 -0800

    Add calc function to calculate total points

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function calc(){
 
+let rank = Number(document.getElementById("rank").value);
+let kills = Number(document.getElementById("kills").value);
+
+let rankPoints = 0;
+
+if(rank==1) rankPoints=10;
+else if(rank==2) rankPoints=6;
+else if(rank==3) rankPoints=5;
+else if(rank<=10) rankPoints=3;
+
+let total = rankPoints + kills;
+
+document.getElementById("result").innerHTML =
+"Total Points: " + total;
+
+}
